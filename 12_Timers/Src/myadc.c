9633989b01b75d6156c9b38b0d4966c07ebb9746
Rogerio Eduardo de Oliveira <rogeduardo@hotmail.com>
2026-06-10 23:17:07 -0300

    Use STM32 Macros

--- 12_Timers/Src/myadc.c
+++ 12_Timers/Src/myadc.c
@@ -1,50 +1,42 @@
 #include "myadc.h"
 
-#define GPIOAEN		(1U<<0)
 //To enable ADC module
-#define ADC1EN		(1U<<8)
-#define ADC_CH1 	(1U<<0)
 #define ADC_SEQ_LEN 0x00
-#define CR2_ADON	(1U<<0)
-#define CR2_SWSTART	(1U<<30)
-#define CR2_CONT	(1U<<1)
-
-#define SR_EOC		(1U<<1)
 
 void pa1_adc_init(void)
 {
 	//Configure the ADC GPIO pin
 
 	//Enable clock access to GPIOA
-	RCC->AHB1ENR |= GPIOAEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
 	//Set the mode of PA1 to analog
-	GPIOA->MODER |= (1U<<2);
-	GPIOA->MODER |= (1U<<3);
+	GPIOA->MODER |= GPIO_MODER_MODE1_0;
+	GPIOA->MODER |= GPIO_MODER_MODE1_1;
 
 	//Configure the ADC module
 	//Enable clock access to ADC
-	RCC->APB2ENR |= ADC1EN;
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 	//Conversion sequence start
-	ADC1->SQR3 |= ADC_CH1;
+	ADC1->SQR3 |= ADC_SQR3_SQ1_0;
 	//Conversion sequence length
 	ADC1->SQR1 = ADC_SEQ_LEN;
 	//Enable ADC module
-	ADC1->CR2 |= CR2_ADON;
+	ADC1->CR2 |= ADC_CR2_ADON;
 }
 
 void start_conversion(void)
 {
 	//Enable continuous conversion
-	ADC1->CR2 |= CR2_CONT;
+	ADC1->CR2 |= ADC_CR2_CONT;
 	//Start adc conversion
-	ADC1->CR2 |= CR2_SWSTART;
+	ADC1->CR2 |= ADC_CR2_SWSTART;
 }
 
 uint32_t adc_read(void)
 {
 	//Wait for conversion to be complete
-	while ( !(ADC1->SR & SR_EOC)){}
+	while ( !(ADC1->SR & ADC_SR_EOC)){}
 	//Read converted result
 	return ADC1->DR;
 }
